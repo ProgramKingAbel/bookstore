ff06d42a3d919e9b7ef5cc147ae2f51f20f487f5
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-23 01:37:39 +0100

    Implement Navigation in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+const links = [
+    { path: '/', text: 'Books' },
+    { path: 'Categories', text: 'Categories' },
+]
+
+const Navbar = () => (
+    <nav>
+        <h1>Bookstore CMS</h1>
+        <ul>
+            {links.map((link) => (
+                <li key={link.text}>
+                    <NavLink to={link.path}>{link.text}</NavLink>
+                </li>
+            ))}
+        </ul>
+    </nav>
+);
+
 
 export default Navbar
