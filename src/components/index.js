df27762f16a6c64301f57f9b1e17277215202e91
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-23 01:17:04 +0100

    Export defaults from components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as BookItem } from './BookItem';
+export { default as BookList } from './BookList';
+export { default as Form } from './Form';
+export { default as Navbar } from './Navbar';
+export { default as Layout } from './Layout';
