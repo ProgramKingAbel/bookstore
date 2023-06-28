61391b7f71633e141eb7cb82c0f1e9757def0aaf
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-28 11:59:44 +0100

    Add reducer to add book in slice

---
+++ src/redux/features/books/booksSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { books: [] };
+
+const bookSlice = createSlice({
+    name: 'book',
+    initialState,
+    reducers: {
+        added: (state, action) => state.books = action.payload
+    }
+})
