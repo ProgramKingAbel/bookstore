0d2b4d3193dd1182709b5b5ec12c22ec191d6409
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-29 00:17:46 +0100

    Create app in backend if it does not exist so far

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,31 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
+import axios from 'axios';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+const appId = localStorage.getItem('appId');
+if (!appId) {
+  axios.post(`${process.env.REACT_APP_MY_BOOKSTORE_URL}`)
+    .then(response => {
+      localStorage.setItem('appId', response.data)
+      renderApp();
+    }).catch(error => {
+      console.log('Error creating app', error)
+      
+    });  
+} else {
+  renderApp();
+}
+
+const renderApp = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
+}
