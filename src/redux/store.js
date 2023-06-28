39a1a35e806916d22d07cbd2d174eb7106fc6a64
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-28 12:25:58 +0100

    Pass reducers to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookReducer from './features/books/booksSlice';
 import categoriesReducer from "./features/categories/categoriesSlice";
+
+const store = configureStore({
+    reducer: {
+        book: bookReducer,
+        category: categoriesReducer,
+    }
+})
+
+export default store;
