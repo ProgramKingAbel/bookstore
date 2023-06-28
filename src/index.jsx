090287ed437f79bed2df9ec8adaf27aa7178d626
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-29 00:36:26 +0100

    Fix linters

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,16 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import axios from 'axios';
 import App from './App';
 import store from './redux/store';
-import axios from 'axios';
 
-const DisplayError = () => {
-  return (
-    <div>
-      <p>Error Creating App. Please try to refresh the page</p>
-    </div>
-  )
-}
+const DisplayError = () => (
+  <div>
+    <p>Error Creating App. Please try to refresh the page</p>
+  </div>
+);
 
 const Index = () => {
   const [error, setError] = useState(false);
@@ -21,12 +19,11 @@ const Index = () => {
     const appId = localStorage.getItem('appId');
     if (!appId) {
       axios.post(`${process.env.REACT_APP_MY_BOOKSTORE_URL}`)
-        .then(response => {
-          localStorage.setItem('appId', response.data)
-        }).catch(error => {
-          console.error('Error creating app', error)
+        .then((response) => {
+          localStorage.setItem('appId', response.data);
+        }).catch((error) => {
+          console.error('Error creating app', error);
           setError(true);
-             
         });
     }
   }, []);
@@ -34,16 +31,15 @@ const Index = () => {
   if (error) {
     return <DisplayError />;
   }
-    return (
-      <React.StrictMode>
-        <BrowserRouter>
-          <Provider store={store}>
-            <App />
-          </Provider>
-        </BrowserRouter>
-      </React.StrictMode>
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </React.StrictMode>
   );
-}
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render( <Index />);
-
+};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Index />);
--- src/redux/features/books/booksSlice.js
+++ src/redux/features/books/booksSlice.js
@@ -1,5 +1,4 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   loading: false,
