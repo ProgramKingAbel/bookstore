d82a0022578c4255c939798360b6171dc0a9e6f5
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-30 00:50:53 +0100

    Implement BookItemUI

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -12,21 +12,23 @@ const BookList = () => {
   }, [dispatch]);
 
   return (
-    <ul>
+    <ul className="wrapper">
       {Object.keys(books).map((key) => (
         books[key].map((book) => (
-          <li key={key}>
+          <li key={key} className="card">
             <div>
 
               <div>
-                <h2>{book.category}</h2>
-                <h1>{book.title}</h1>
-                <h3>{book.author}</h3>
+                <h2 className="category">{book.category}</h2>
+                <h1 className="title">{book.title}</h1>
+                <h3 className="author">{book.author}</h3>
               </div>
 
-              <div>
+              <div className="actions">
                 <button type="button">Comments</button>
+                <hr />
                 <button type="button" onClick={() => { dispatch(deleteBook(key)); }}>Remove</button>
+                <hr />
                 <button type="button">Edit</button>
               </div>
 
@@ -35,9 +37,9 @@ const BookList = () => {
             <div>60%</div>
 
             <div>
-              <h3>Current Chapter</h3>
-              <h4>Chapter 8</h4>
-              <button type="button">Update Progress</button>
+              <h3 className="chapter">Current Chapter</h3>
+              <h4 className="lesson">Chapter 8</h4>
+              <button className="update-progress" type="button">UPDATE PROGRESS</button>
             </div>
 
           </li>
