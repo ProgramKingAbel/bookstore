4246c7690c3053353fc3bd610701d7847e63c630
ProgramKingAbel <abelmorara254@gmail.com>
2023-06-30 15:51:12 +0100

    Add Under construction for categories

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import '../styles/Categories.css';
 
 const Categories = () => (
-  <div>Categories</div>
+  <div className="categories">
+    <h1>Site Under construction!</h1>
+    <img src="../assets/images/images.png" alt="under construction" />
+  </div>
 );
 
 export default Categories;
