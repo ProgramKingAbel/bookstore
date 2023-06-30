0a95c9fc7e725c5684eedc2e9f4bd5f20443666e
Abel Matunda Morara <abelmorara254@gmail.com>
2023-06-30 16:13:54 +0100

    Update Categories.jsx

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -4,7 +4,6 @@ import '../styles/Categories.css';
 const Categories = () => (
   <div className="categories">
     <h1>Site Under construction!</h1>
-    <img src="../assets/images/images.png" alt="under construction" />
   </div>
 );
 
